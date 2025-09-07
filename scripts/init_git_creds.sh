58dcc20e9811541ad3af5dde0ad4721988059b4e
JC-Hall <jch7aa@irginia.edu>
2025-09-07 20:56:01 +0000

    init git creds

---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=jch7aa@irginia.edu
+NAME=JC-Hall
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
