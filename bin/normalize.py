ca1230c7ba3593092b3fce80d1554abee3985a53
JC-Hall <jch7aa@irginia.edu>
2025-09-17 06:47:47 +0000

    Updated-fully clean

--- bin/normalize.py
+++ bin/normalize.py
@@ -1,19 +1,40 @@
 import pandas as pd
 import numpy as np
 import re
+import sys
 
-def normalize_y():
-	df = pd.read_csv('ygainers.csv')
-	df['entry_price'] = df['Price'].astype(str).apply(lambda x: str(x).split()[0])
-	df['entry_price'] = pd.to_numeric(df['entry_price'], errors='coerce')
-	print(df[['Price', 'entry_price']].head())
 
-def normalize_wsj():
-	df = pd.read_csv('wsjgainers.csv')
+def normalize_y(fname):
+	df = pd.read_csv(fname)
+	df['symbol'] = df['Symbol']
+	df['company_name'] = df['Name']
+	df['price'] = df['Price'].astype(str).apply(lambda x: str(x).split()[0])
+	df['price'] = pd.to_numeric(df['price'], errors='coerce')
+	df['change'] = df['Change']
+	df['perc_change'] = df['Change %'].astype(str).str.replace('%', '', regex=False).str.replace('+', '', regex=False)
+	df['volume'] = df['Volume']
+
+	final = df[['symbol', 'company_name', 'price', 'change', 'perc_change', 'volume']].dropna()
+	print(final)
+
+def normalize_wsj(fname):
+	df = pd.read_csv(fname)
 	rex = r'\(([A-Z]+)\)$'
-	df['sym'] = df['Unnamed: 0'].astype(str).apply(lambda x: re.findall(rex, x)[0])
-	print(df[['Unnamed: 0', 'sym']].head())
+	df['symbol'] = df['Unnamed: 0'].astype(str).apply(lambda x: re.findall(rex, x)[0])
+	df['company_name'] = df['Unnamed: 0'].astype(str).apply(lambda x: re.findall(rex, x)[0])
+	df['price'] = df['Last']
+	df['change'] = df['Chg']
+	df['perc_change'] = df['% Chg']
+	df['volume'] = df['Volume']
+
+	final = df[['symbol', 'company_name', 'price', 'change', 'perc_change', 'volume']].dropna()
+	print(final)	
+
 
 if __name__ == '__main__':
-	normalize_y()
-	normalize_wsj()
+	fname = sys.argv[1]
+
+	if fname.startswith('y'):
+		normalize_y(fname)
+	else:
+		normalize_wsj(fname)
