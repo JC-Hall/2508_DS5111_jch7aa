de376f52226a0dde036e91dcfccace568083c4c7
JC-Hall <jch7aa@irginia.edu>
2025-10-02 08:33:22 +0000

    add test to check os and python version 3.12, 3.13

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -16,3 +16,13 @@ def test_os():
     print(f"OS platform: {platform.system}")
     assert platform.system()
 
+def test_linux_run():
+    """Fail if the OS is not Linux."""
+    os = platform.system()
+    assert os == "Linux", f"This isnt Linux, this is {os}"
+
+def test_specific_python_v():
+    """Fail if Python version is not 3.12 or 3.13."""
+    v1, v2 = sys.version_info[:2]
+    allowed_v = [(3, 12), (3, 13)]
+    assert (v1, v2) in allowed_v
