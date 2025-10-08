49291302aa923d988b937eff3713e1bdaa206627
JC-Hall <jch7aa@irginia.edu>
2025-10-08 00:36:14 -0400

    Crontab jobs for mon-fri stock scrap

---
+++ data-collection-cron-jobs.sh
@@ -0,0 +1,4 @@
+31 9 * * 1-5 cd 2508_DS5111_jch7aa/; make clean; make ygainers_tstamp; make wsjgainers_tstamp; make clean
+30 12 * * 1-5 cd 2508_DS5111_jch7aa/; make clean; make ygainers_tstamp; make wsjgainers_tstamp; make clean
+1 16 * * 1-5 cd 2508_DS5111_jch7aa/; make clean; make ygainers_tstamp; make wsjgainers_tstamp; make clean 
+
